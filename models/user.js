4642fe644151ecf24205b3d61a1481e128d08318
K0K0K7 <keerthik4444@gmail.com>
2020-06-26 18:30:10 +0530

    Added models :star:

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose');
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    workspaces: [{
+        type: mongoose.Schema.Types.ObjectId,
+            ref: 'WorkSpace'
+    }],
+    salt: {
+        type: String,
+        required: true
+    },
+    createdDate: {
+        type: Date
+    },
+    updatedDate:{
+        type: Date,
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
