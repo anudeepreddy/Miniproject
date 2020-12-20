4542a58b0a2f10e24ffb00d8d883517b76ea348d
Anudeep Reddy <anudeepc85@gmail.com>
2020-12-20 11:29:59 +0530

    workspace will redirect to login if used is not logged in

--- frontend/src/pages/Workspace.js
+++ frontend/src/pages/Workspace.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import {Col, Layout, Row} from 'antd';
 import HeaderComponent from 'components/HeaderComponent';
 import WorkspaceSidebar from 'components/workspace/WorkspaceSidebar';
@@ -10,29 +11,31 @@ import "react-toastify/dist/ReactToastify.css";
 const {Content} = Layout;
 
 function Workspace(props) {
-    if(props.isLoggedIn)
     return (
-        <Layout>
-            <HeaderComponent username="Anudeep" workspaceName="mark1"/>
-            <Content>
-                <Row>
-                    <Col span={19}>
-                        <WorkspaceContent language='javascript'/>
-                    </Col>
-                    <Col span={5}>
-                        <WorkspaceSidebar language={{name: "C++", value: "cpp"}}/>
-                    </Col>
-                </Row>
-            </Content>
-        </Layout>
-    );
-    else
-    return (
-        <div style={{textAlign: 'center'}}>
-        <h1>Login to Continue</h1>
-        <a  href="/login">Login Here!</a> 
-       </div>
-    );
+        <>
+            {
+                props.isLoggedIn?
+                (
+                    <Layout>
+                        <HeaderComponent username="Anudeep" workspaceName="mark1"/>
+                        <Content>
+                            <Row>
+                                <Col span={19}>
+                                    <WorkspaceContent language='javascript'/>
+                                </Col>
+                                <Col span={5}>
+                                    <WorkspaceSidebar language={{name: "C++", value: "cpp"}}/>
+                                </Col>
+                            </Row>
+                        </Content>
+                    </Layout>
+                ):
+                (
+                    <Redirect to="/login"/>
+                )
+            }
+        </>
+    )
 }
 
 
